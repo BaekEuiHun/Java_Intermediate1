ae499a23e9d7ed3dc543e254142587414f2c371c
BaekEuiHun <euihunbaek@gmail.com>
2024-06-27 15:50:37 +0900

    init

---
+++ src/lang/object/tostring/TOStringMain1.java
@@ -0,0 +1,13 @@
+package lang.object.tostring;
+
+public class TOStringMain1 {
+    public static void main(String[] args) {
+        Object object = new Object();
+        String string = object.toString();
+
+        System.out.println(object);
+        System.out.println(string);
+    }
+
+
+}

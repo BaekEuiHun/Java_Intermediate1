bc85e02cc4134945009f669592e02f01c9d79eb7
BaekEuiHun <euihunbaek@gmail.com>
2024-07-01 09:58:10 +0900

    init

--- src/lang/immutable/address/RefMain2.java
+++ src/lang/immutable/address/RefMain2.java
@@ -8,7 +8,8 @@ public class RefMain2 {
         System.out.println("a = " + a);
         System.out.println("b = " + b);
 
-        b.setValue("부산");
+
+        b = new ImmutableAddress("부산");
         System.out.println("부산 -> b");
         System.out.println("a = " + a);
         System.out.println("b = " + b);

6c0d885adbfcd2ccd2ad93f83d1ae340dfb8f27a
BaekEuiHun <euihunbaek@gmail.com>
2024-07-03 16:44:38 +0900

    init

--- src/enumeration/ex2/ClassGrade.java
+++ src/enumeration/ex2/ClassGrade.java
@@ -5,5 +5,10 @@ public class ClassGrade {
     public static final ClassGrade GOLD = new ClassGrade();
     public static final ClassGrade DIAMOND = new ClassGrade();
 
+    //private 생성자 추가
+    private ClassGrade() {
+
+    }
+
 }
 

28429db9bc35c84dbec0bdf741418bec05a62967
BaekEuiHun <euihunbaek@gmail.com>
2024-07-04 17:04:20 +0900

    init

---
+++ src/enumeration/ref2/EnumRefMain2.java
@@ -0,0 +1,17 @@
+package enumeration.ref2;
+
+
+public class EnumRefMain2 {
+    public static void main(String[] args) {
+        int price = 10000;
+        Grade[] grades = Grade.values();
+        for (Grade grade : grades) {
+            print(grade, price);
+
+        }
+    }
+
+    private static void print(Grade grade, int price) {
+        System.out.println(grade.name() + " 등급의 할인 금액 : " + grade.discount(price));
+    }
+}

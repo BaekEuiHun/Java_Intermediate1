c744ec8a32d01e9ff15e9b093b55e609f5e4dcdd
EuiHun_Bori <euihunbaek@gmail.com>
2024-07-22 21:12:01 +0900

    Update Client.java

--- src/exception/basic/checked/Client.java
+++ src/exception/basic/checked/Client.java
@@ -1,7 +1,7 @@
 package exception.basic.checked;
 
 public class Client {
-    public void call() throws MycheckedException{
+    public void call() throws  MycheckedException{
 
         throw new MycheckedException("ex");
     }

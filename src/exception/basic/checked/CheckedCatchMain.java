41b7169c69a2c5356e53d679a3d2ed58de87f03e
EuiHun_Bori <euihunbaek@gmail.com>
2024-07-22 21:11:48 +0900

    Update CheckedCatchMain.java

--- src/exception/basic/checked/CheckedCatchMain.java
+++ src/exception/basic/checked/CheckedCatchMain.java
@@ -4,6 +4,6 @@ public class CheckedCatchMain {
     public static void main(String[] args) {
         Service service = new Service();
         service.callCatch();
-        System.out.println("정상종료");
+        System.out.println("정상 종료");
     }
 }

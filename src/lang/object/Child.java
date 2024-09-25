4e5898cbefd087bdb439d2bea449ab973cb98f70
BaekEuiHun <euihunbaek@gmail.com>
2024-09-25 19:59:55 +0900

    묵시적 명시적 상속 명시

--- src/lang/object/Child.java
+++ src/lang/object/Child.java
@@ -2,6 +2,8 @@ package lang.object;
 
 public class Child extends Parent {
 
+
+    //명시적으로 상속받았으므로 Object 상속 X
     public void childMethod() {
         System.out.println("Child.childMethod");
     }

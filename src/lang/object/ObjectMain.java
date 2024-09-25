c2bbe934cdf6bcf832dcb3e1b3e89f8451d54441
BaekEuiHun <euihunbaek@gmail.com>
2024-09-25 20:06:44 +0900

    실행 method 생성

--- src/lang/object/ObjectMain.java
+++ src/lang/object/ObjectMain.java
@@ -5,7 +5,10 @@ public class ObjectMain {
         Child child = new Child();
         child.childMethod();
         child.parentMethod();
+
         //toString()은 Object 클래스의 메서드
+
+        //자바에서 모든 객체의 최종 부모는 Object이다.
         String string = child.toString();
         System.out.println(string);
     }

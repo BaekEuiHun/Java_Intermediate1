ad18b511be2a02ce4100589a844c22d5fd05e7b7
BaekEuiHun <euihunbaek@gmail.com>
2024-09-26 12:09:36 +0900

    코드정렬

--- src/lang/immutable/address/PrimitiveMain.java
+++ src/lang/immutable/address/PrimitiveMain.java
@@ -2,8 +2,8 @@ package lang.immutable.address;
 
 public class PrimitiveMain {
     public static void main(String[] args) {
-        //기본형은 절대로 같은 값을 공유하지 않는다.
 
+        //기본형은 절대로 같은 값을 공유하지 않는다.
         int a = 10;
         int b = a;
         System.out.println("a = " + a);

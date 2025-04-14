1d8b0af9d9a6526e8bf37ff52189c72aca210769
EuiHun_Bori <euihunbaek@gmail.com>
2025-04-14 21:30:11 +0900

    Update NetworkMain.java

--- src/nested/nested/ex2/NetworkMain.java
+++ src/nested/nested/ex2/NetworkMain.java
@@ -3,6 +3,7 @@ package nested.nested.ex2;
 
 public class NetworkMain {
     public static void main(String[] args) {
+        
         Network network = new Network();
         network.sendMessage("Hello World");
     }
